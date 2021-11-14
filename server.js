911e4a0a26b2583b0a2f41e48b960c857eb4b435
hvardhan878 <hvardhan878@gmail.com>
2021-11-14 18:07:09 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+
+app.set('view engine','ejs')
+
+app.use(express.static(__dirname + '/views/'))
+
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+
+app.listen(8080,()=>{
+    console.log("Listening to 8080")
+})
